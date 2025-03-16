19262b159195a94e9ffa5be5b1826aa8122f95a5
kata <omarmohamedx4e@gmail.com>
2025-03-16 22:25:25 +0200

    add fault report specs

---
+++ src/VMTS.Core/Specifications/FaultReportSepcification/FaultReportSpecParams.cs
@@ -0,0 +1,36 @@
+﻿using VMTS.Core.Entities.Maintenace;
+using VMTS.Core.Entities.Report;
+
+namespace VMTS.Core.Specifications.FaultReportSepcification;
+
+public class FaultReportSpecParams
+{
+    public string? TripId { get; set; }
+
+    public string? VehicleId { get; set; }
+
+    public MaintenanceType? FaultType { get; set; }
+
+    public DateTime? ReportDate { get; set; }
+
+    private int pagesize = 10;
+    private const int maxsize = 20;
+
+    public int PageIndex { get; set; }
+
+    public int PageSize
+    {
+        get { return pagesize; }
+        set { pagesize = value > maxsize ? maxsize : value; }
+    }
+
+    private string? search;
+
+    public string? Search
+    {
+        get { return search; }
+        set { search = value; }
+    }
+
+    public string? Sort { get; set; }
+}
