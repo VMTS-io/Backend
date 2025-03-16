ec6f1e6924f444e3861e7114f5350b2c368dba04
kata <omarmohamedx4e@gmail.com>
2025-03-16 22:24:18 +0200

    add fault report controller and "create" endpoint

---
+++ src/VMTS.API/Controllers/FaultReportController.cs
@@ -0,0 +1,54 @@
+﻿using System.Security.Claims;
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using VMTS.API.Dtos;
+using VMTS.API.Errors;
+using VMTS.Core.Entities.Identity;
+using VMTS.Core.ServicesContract;
+using VMTS.Service.Services;
+
+namespace VMTS.API.Controllers;
+
+
+public class FaultReportController : BaseApiController
+{
+    private readonly IReportService _ireportService;
+    private readonly IMapper _mapper;
+
+
+    public FaultReportController(
+        IReportService ireportService,
+        IMapper mapper)
+    {
+        _ireportService = ireportService;
+        _mapper = mapper;
+    }
+   
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [HttpPost("create")]
+    public async Task<ActionResult<FaultReportResponse>> CreateFaultReportAsync(FaultReportRequest request)
+    {
+        var user = User.FindFirstValue(ClaimTypes.Email);
+        if (string.IsNullOrEmpty(user))
+            return Unauthorized(new ApiResponse(401));
+
+        var faultReport = await _ireportService.CreateFaultReportAsync(
+            user,
+            request.Details,
+            request.FaultType,
+            request.FaultComponent,
+            request.Address
+        );
+    
+        if (faultReport is null)
+            return BadRequest(new ApiResponse(400, "Failed to create fault report"));
+
+        var mappedReport = _mapper.Map<FaultReportResponse>(faultReport);
+        return Ok(mappedReport);
+    }
+
+
+}
