e9f4f0016d919032dd49047c6c0d7c73ab4220fe
kata <omarmohamedx4e@gmail.com>
2025-03-16 22:14:18 +0200

    add business user config

---
+++ src/VMTS.Repository/Data/Config/BusinessUserConfig.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VMTS.Core.Entities.User_Business;
+
+namespace VMTS.Repository.Data.Config;
+
+public class BusinessUserConfig : IEntityTypeConfiguration<BusinessUser>
+{
+    public void Configure(EntityTypeBuilder<BusinessUser> builder)
+    {
+
+        #region TripRequest
+        builder.HasMany(m => m.ManagerTripRequest)
+            .WithOne(t => t.Manager)
+            .HasForeignKey(t => t.ManagerId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasMany(m => m.DriverTripRequest)
+            .WithOne(t => t.Driver)
+            .HasForeignKey(t => t.DriverId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Restrict);
+            
+        #endregion
+
+        #region Trip Reports
+            builder.HasMany(m => m.DriverTripReport)
+            .WithOne(t => t.Driver)
+            .HasForeignKey("DriverId")
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Restrict);
+        #endregion
+
+        #region FaultReport
+            builder.HasMany(m => m.DriverFaultReport)
+                .WithOne(t => t.Driver)
+                .HasForeignKey("DriverId")
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+        #endregion
+
+        #region  MaintenaceReports
+        builder.HasMany(b => b.MechanicMaintenaceReports)
+            .WithOne(m => m.Mechanic)
+            .HasForeignKey("MechanicId")
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Restrict);
+        #endregion
+
+        #region MaintenanceRequest
+        builder.HasMany(b => b.ManagerMaintenaceRequests)
+            .WithOne(m => m.ManagerMaintenanceRequest)
+            .HasForeignKey("MechanicId")
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Restrict);
+        
+
+        #endregion
+
+    }
+}
