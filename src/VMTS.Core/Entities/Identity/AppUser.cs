35ae72c7e614d293954a6493ab8814c909f88ac2
kata <omarmohamedx4e@gmail.com>
2025-03-16 22:06:19 +0200

    remove Display Name

--- src/VMTS.Core/Entities/Identity/AppUser.cs
+++ src/VMTS.Core/Entities/Identity/AppUser.cs
@@ -4,9 +4,6 @@ namespace VMTS.Core.Entities.Identity;
 
 public class AppUser : IdentityUser 
 {
-    public string DisplayName { get; set; }
-    
     public Address Address { get; set; }
-    
     public bool MustChangePassword { get; set; } = true;
 }
