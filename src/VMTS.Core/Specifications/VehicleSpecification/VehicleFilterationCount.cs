1764010cc80e57347e2e16d1e70d3137a810fd51
kata <omarmohamedx4e@gmail.com>
2025-03-16 22:39:58 +0200

    add vehicle specs

---
+++ src/VMTS.Core/Specifications/VehicleSpecification/VehicleFilterationCount.cs
@@ -0,0 +1,18 @@
+﻿using VMTS.Core.Entities;
+using VMTS.Core.Entities.Vehicle_Aggregate;
+
+namespace VMTS.Core.Specifications.VehicleSpecification;
+
+public class VehicleFilterationCount : BaseSpecification<Vehicle>
+{
+    public VehicleFilterationCount(VehicleSpecParams specParams) : base(v =>
+    string.IsNullOrEmpty(specParams.Search) || v.Name.ToLower().Contains(specParams.Search) &&
+    (string.IsNullOrEmpty(specParams.CategoryId) || v.CategoryId == specParams.CategoryId) &&
+    (string.IsNullOrEmpty(specParams.ModelId) || v.ModelId == specParams.ModelId))
+    {
+        
+    }
+
+
+
+}
