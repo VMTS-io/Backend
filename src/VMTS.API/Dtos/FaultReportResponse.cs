50bb40e983d9d5f61ee18fbdcb945762b3eeaf40
kata <omarmohamedx4e@gmail.com>
2025-03-16 22:34:20 +0200

    add fault report dto

---
+++ src/VMTS.API/Dtos/FaultReportResponse.cs
@@ -0,0 +1,20 @@
+﻿using VMTS.Core.Entities.Maintenace;
+using VMTS.Core.Entities.Report;
+
+namespace VMTS.API.Dtos;
+
+public class FaultReportResponse
+{
+    public string DriverId { get; set; } 
+    public string TripId { get; set; }
+    public string Destination { get; set; }
+    public string FaultAddress { get; set; } = string.Empty;
+    public string Details { get; set; } = string.Empty;
+    public MaintenanceType FaultType { get; set; }  
+    public FaultComponent? FaultComponent { get; set; }
+    public FaultReportStatus Status { get; set; } = FaultReportStatus.Reported;
+    public DateTime CreatedAt { get; set; }
+    
+    
+    
+}
