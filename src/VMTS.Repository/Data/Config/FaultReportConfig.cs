45c62eb672e39dad65f3bc0329848bde6326e710
kata <omarmohamedx4e@gmail.com>
2025-03-16 22:23:19 +0200

    add fault report config

---
+++ src/VMTS.Repository/Data/Config/FaultReportConfig.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VMTS.Core.Entities.Report;
+
+namespace VMTS.Repository.Data.Config;
+
+public class FaultReportConfig : IEntityTypeConfiguration<FaultReport>
+{
+    public void Configure(EntityTypeBuilder<FaultReport> builder)
+    {
+        builder.HasOne(f => f.Trip)
+               .WithMany(t => t.FaultReports)
+               .HasForeignKey(f => f.TripId)
+               .OnDelete(DeleteBehavior.Restrict);
+    }
+}
