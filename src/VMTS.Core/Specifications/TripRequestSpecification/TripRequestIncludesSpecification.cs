baaaf3b1fb525ce2ea3ad29db4c77702b1ec5467
kata <omarmohamedx4e@gmail.com>
2025-03-16 22:45:13 +0200

    add trip request specs

---
+++ src/VMTS.Core/Specifications/TripRequestSpecification/TripRequestIncludesSpecification.cs
@@ -0,0 +1,23 @@
+﻿using VMTS.Core.Entities.Trip;
+
+namespace VMTS.Core.Specifications.TripRequestSpecification;
+
+public class TripRequestIncludesSpecification : BaseSpecification<TripRequest>
+{
+        public TripRequestIncludesSpecification(string driverId) 
+            : base(t => 
+                t.Driver != null && t.DriverId == driverId &&
+                t.Status == TripStatus.Approved &&
+                t.Date <= DateTime.UtcNow)
+        {
+            ApplyIncludes();
+        }
+
+        private void ApplyIncludes()
+        {
+            Includes.Add(t => t.Vehicle);
+            Includes.Add(t => t.Driver);  
+        }
+    }
+
+ 
