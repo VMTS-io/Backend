3277ff91d45ad2247edd5158e58b392dbfdea936
kata <omarmohamedx4e@gmail.com>
2025-03-16 22:34:32 +0200

    add address dto

---
+++ src/VMTS.API/Dtos/AddressDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VMTS.API.Dtos;
+
+public class AddressDto
+{
+    [Required]
+    public string Street { get; set; } // Example: "El Tahrir St."
+    
+    [Required]
+    public string Area { get; set; } // Example: "Nasr City, Maadi, Mohandessin"
+    
+    [Required]
+    public string Governorate { get; set; } // Example: "Cairo, Giza, Alexandria"
+    
+    [Required]
+    public string Country { get; set; }  // Default value since your system is for Egypt
+}
