24d3b0a5b948b3e290d9bbeea667693452a85233
kata <omarmohamedx4e@gmail.com>
2025-03-16 22:45:54 +0200

    update vehicle entities

--- src/VMTS.Core/Entities/Vehicle_Aggregate/VehicleCategory.cs
+++ src/VMTS.Core/Entities/Vehicle_Aggregate/VehicleCategory.cs
@@ -5,4 +5,8 @@ public class VehicleCategory : BaseEntity
     public string Name { get; set; }
 
     public string Description { get; set; }
+    
+    
+    public ICollection<Vehicle> Vehicle { get; set; }  = new HashSet<Vehicle>();
+
 }
