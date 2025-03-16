8743ce62527d47898c5870b3a48889b5a482de4f
kata <omarmohamedx4e@gmail.com>
2025-03-16 22:37:11 +0200

    update trip report table

--- src/VMTS.Core/Entities/Trip/TripReport.cs
+++ src/VMTS.Core/Entities/Trip/TripReport.cs
@@ -1,4 +1,7 @@
-﻿namespace VMTS.Core.Entities.Trip;
+﻿using VMTS.Core.Entities.User_Business;
+using VMTS.Core.Entities.Vehicle_Aggregate;
+
+namespace VMTS.Core.Entities.Trip;
 
 public class TripReport : BaseEntity
 {
@@ -9,4 +12,13 @@ public class TripReport : BaseEntity
     public DateTime Date { get; set; }
 
     public decimal FuelCost { get; set; }
+
+    // public bool IsFault {get; set; } = false;
+    //
+    // public FaultTypes FaultType { get; set; }
+    
+    public BusinessUser Driver { get; set; }
+    
+    public Vehicle Vehicle { get; set; }
+
 }
