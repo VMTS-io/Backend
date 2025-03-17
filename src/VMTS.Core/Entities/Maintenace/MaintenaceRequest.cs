a1b88445d78d06d17ca875a1895f46a3ad8f5365
Bassel Raafat <basselr11@gmail.com>
2025-03-18 01:30:02 +0200

    Updated some properties

--- src/VMTS.Core/Entities/Maintenace/MaintenaceRequest.cs
+++ src/VMTS.Core/Entities/Maintenace/MaintenaceRequest.cs
@@ -1,4 +1,4 @@
-﻿using VMTS.Core.Entities.User_Business;
+using VMTS.Core.Entities.User_Business;
 using VMTS.Core.Entities.Vehicle_Aggregate;
 
 namespace VMTS.Core.Entities.Maintenace;
@@ -7,12 +7,19 @@ public class MaintenaceRequest : BaseEntity
 {
     public string Description { get; set; }
 
-    public DateTime Date { get; set; }
+    public DateTime Date { get; set; } = DateTime.UtcNow;
 
-    public Status Status { get; set; }
+    public Status Status { get; set; } = Status.Pending;
 
-    public BusinessUser ManagerMaintenanceRequest { get; set; }
-    
+    public BusinessUser Manager { get; set; }
+
+    public string ManagerId { get; set; }
+
+    public BusinessUser Mechanic { get; set; }
+
+    public string MechanicId { get; set; }
+
     public Vehicle Vehicle { get; set; }
 
+    public string VehicleId { get; set; }
 }
