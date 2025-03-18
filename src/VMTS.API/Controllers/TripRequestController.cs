ad53d75993dafa8c8de2bd829137c2161f648ff8
kata <omarmohamedx4e@gmail.com>
2025-03-18 16:05:38 +0200

    add trip request controller

---
+++ src/VMTS.API/Controllers/TripRequestController.cs
@@ -0,0 +1,59 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using VMTS.API.Dtos;
+using VMTS.API.Errors;
+using VMTS.Core.ServicesContract;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VMTS.API.Controllers;
+
+public class TripRequestController : BaseApiController
+{
+    private readonly ITripRequestService _requestService;
+
+    public TripRequestController(ITripRequestService requestService)
+    {
+        _requestService = requestService;
+    }
+    
+
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Manager")]
+    [HttpPost("create")]
+    public async Task<ActionResult<TripRequestResponse>> CreateTripRequestAsync(TripRequestDto request)
+    {
+        var manager =User.FindFirstValue(ClaimTypes.Email);
+        if (string.IsNullOrEmpty(manager))
+            return Unauthorized(new ApiResponse(401));
+        
+        var tripRequest = await _requestService.CreateTripRequestAsync(
+            manager,
+            request.DriverEmail,
+            request.VehicleId,
+            request.TripType,
+            request.Details,
+            request.Destination
+        );
+
+        if (tripRequest is null)
+            return BadRequest(new ApiResponse(400, "Failed to create trip request"));
+
+        var response = new TripRequestResponse
+        {
+            TripType = tripRequest.Type,
+            Details = tripRequest.Details,
+            Destination = tripRequest.Destination,
+            Date = tripRequest.Date,
+            TripStatus = tripRequest.Status,
+            DriverId = tripRequest.DriverId,
+            ManagerId = tripRequest.ManagerId,
+            VehicleId = tripRequest.Vehicle.Id
+        };
+
+        return Ok(response);
+    }
+    
+}
