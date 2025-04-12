c67e11ac81794d146c12db53e8fc1d538eb741ec
Bassel Raafat <basselr11@gmail.com>
2025-04-12 13:46:06 +0200

    Adding data seed to Main dbContext

---
+++ src/VMTS.Repository/Data/VMTSDataSeed.cs
@@ -0,0 +1,19 @@
+using System.Text.Json;
+using VMTS.Core.Entities.Vehicle_Aggregate;
+
+namespace VMTS.Repository.Data;
+
+public static class VMTSDataSeed
+{
+    public static async Task SeedAsync(VTMSDbContext dbContext)
+    {
+        if (!dbContext.Set<Vehicle>().Any())
+        {
+            var vehicleText = File.ReadAllText("../../VMTS.Repository/Data/DataSeed/Vehicel.json");
+            var vehicles = JsonSerializer.Deserialize<List<Vehicle>>(vehicleText);
+            if (vehicles!.Count > 0)
+                await dbContext.AddRangeAsync(vehicles);
+            dbContext.SaveChanges();
+        }
+    }
+}
