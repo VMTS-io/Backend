d95e85679b92a0195ae67cdff6242539a51a98ad
Bassel Raafat <basselr11@gmail.com>
2025-04-17 21:14:50 +0200

    Removed Name and added LastDateAssigned

--- src/VMTS.Core/Entities/Vehicle_Aggregate/Vehicle.cs
+++ src/VMTS.Core/Entities/Vehicle_Aggregate/Vehicle.cs
@@ -5,8 +5,6 @@ namespace VMTS.Core.Entities.Vehicle_Aggregate;
 
 public class Vehicle : BaseEntity
 {
-    public string Name { get; set; }
-
     public string PalletNumber { get; set; }
 
     public DateTime JoindYear { get; set; }
@@ -15,19 +13,19 @@ public class Vehicle : BaseEntity
 
     public int KMDriven { get; set; }
 
-    public VehicleStatus Status { get; set; }
+    public VehicleStatus Status { get; set; } = VehicleStatus.Active;
 
-
+    public DateTime? LastAssignedDate { get; set; } = DateTime.UtcNow;
     public string ModelId { get; set; }
     public string CategoryId { get; set; }
 
-    
     public VehicleModel VehicleModel { get; set; }
     public VehicleCategory VehicleCategory { get; set; }
-    
+
     public ICollection<TripRequest> TripRequests { get; set; } = new HashSet<TripRequest>();
     public ICollection<TripReport> TripReports { get; set; } = new HashSet<TripReport>();
-    public ICollection<MaintenaceReport> MaintenaceReports { get; set; }
-    public ICollection<MaintenaceRequest> MaintenaceRequests { get; set; }
-    
+    public ICollection<MaintenaceReport> MaintenaceReports { get; set; } =
+        new HashSet<MaintenaceReport>();
+    public ICollection<MaintenaceRequest> MaintenaceRequests { get; set; } =
+        new HashSet<MaintenaceRequest>();
 }
