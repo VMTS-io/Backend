82c43a79940b6c0709a8c127d313e57b499275e5
kata <omarmohamedx4e@gmail.com>
2025-04-19 03:03:13 +0200

    add some properties

--- src/VMTS.Core/Entities/Identity/AppUser.cs
+++ src/VMTS.Core/Entities/Identity/AppUser.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Runtime.InteropServices.JavaScript;
+using Microsoft.AspNetCore.Identity;
 
 namespace VMTS.Core.Entities.Identity;
 
 public class AppUser : IdentityUser 
 {
+    public string DisplayName { get; set; }
     public Address Address { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+    public string NationalId { get; set; }
+    
+    public string PictureUrl { get; set; }  
     public bool MustChangePassword { get; set; } = true;
 }
