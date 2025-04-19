968b02031202478cc9c351e28de9d59e2c9b5fc6
kata <omarmohamedx4e@gmail.com>
2025-04-19 23:48:56 +0200

    fix login

--- src/VMTS.API/Dtos/User/MustChangePasswordDto.cs
+++ src/VMTS.API/Dtos/User/MustChangePasswordDto.cs
@@ -5,6 +5,8 @@ namespace VMTS.API.Dtos;
 public class MustChangePasswordDto
 {
     public bool MustChangePassword { get; set; }
+    public int StatusCode { get; set; }
+    public string Message { get; set; }
 
-    public ApiResponse ApiResponse { get; set; }
+
 }
