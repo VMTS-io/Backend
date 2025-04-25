10b7d90bb927dee9bff01ae8418d3d7f12afdac9
kata <omarmohamedx4e@gmail.com>
2025-04-25 21:55:01 +0300

    add send email request

---
+++ src/VMTS.API/Dtos/User/SendEmailRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace VMTS.API.Dtos;
+
+public class SendEmailRequest
+{
+    public string toEmail { get; set; }
+    public string subject { get; set; }
+    public string body { get; set; }
+    public bool isHtml = true;
+}
