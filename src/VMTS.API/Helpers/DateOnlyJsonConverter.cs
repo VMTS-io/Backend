01db3d6793f713c48d562f632a0cccf860e8ce29
kata <omarmohamedx4e@gmail.com>
2025-04-29 23:42:37 +0300

    update date form

--- src/VMTS.API/Helpers/DateOnlyJsonConverter.cs
+++ src/VMTS.API/Helpers/DateOnlyJsonConverter.cs
@@ -1,19 +1,46 @@
-﻿using System.Text.Json;
+﻿using System.Globalization;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
-namespace VMTS.API.Helpers;
-
-public class DateOnlyJsonConverter : JsonConverter<DateOnly>
+namespace VMTS.API.Helpers
 {
-    private const string Format = "yyyy-MM-dd";
-
-    public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    public class DateOnlyJsonConverter : JsonConverter<DateOnly>
     {
-        return DateOnly.ParseExact(reader.GetString()!, Format);
-    }
+        private static readonly string[] SupportedFormats = { "yyyy-MM-dd", "yyyy/MM/dd" };
 
-    public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options)
-    {
-        writer.WriteStringValue(value.ToString(Format));
+        public override DateOnly Read(
+            ref Utf8JsonReader reader,
+            Type typeToConvert,
+            JsonSerializerOptions options
+        )
+        {
+            string dateString = reader.GetString();
+            foreach (var format in SupportedFormats)
+            {
+                if (
+                    DateOnly.TryParseExact(
+                        dateString,
+                        format,
+                        CultureInfo.InvariantCulture,
+                        DateTimeStyles.None,
+                        out var result
+                    )
+                )
+                {
+                    return result;
+                }
+            }
+
+            throw new FormatException($"Invalid date format: {dateString}");
+        }
+
+        public override void Write(
+            Utf8JsonWriter writer,
+            DateOnly value,
+            JsonSerializerOptions options
+        )
+        {
+            writer.WriteStringValue(value.ToString("yyyy-MM-dd")); // You can choose any format for writing back.
+        }
     }
 }
