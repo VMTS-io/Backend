dd0ead56fc0cb5a40319d68ec10b11138130aed7
kata <omarmohamedx4e@gmail.com>
2025-04-29 23:40:12 +0300

    add first and last name

--- src/VMTS.Core/Entities/Identity/AppUser.cs
+++ src/VMTS.Core/Entities/Identity/AppUser.cs
@@ -3,13 +3,16 @@ using Microsoft.AspNetCore.Identity;
 
 namespace VMTS.Core.Entities.Identity;
 
-public class AppUser : IdentityUser 
+public class AppUser : IdentityUser
 {
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
     public string DisplayName { get; set; }
     public Address Address { get; set; }
     public DateOnly DateOfBirth { get; set; }
     public string NationalId { get; set; }
-    
-    public string PictureUrl { get; set; }  
+
+    public string PictureUrl { get; set; }
     public bool MustChangePassword { get; set; } = true;
 }
