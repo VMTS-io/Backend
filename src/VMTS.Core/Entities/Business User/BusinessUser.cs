2658bef58b1d2035ce847c537196a26c22a1ccc8
Omar Mohamed <omarmohamedx4e@gmail.com>
2025-06-18 00:00:26 +0300

    add role to BU

--- src/VMTS.Core/Entities/Business User/BusinessUser.cs
+++ src/VMTS.Core/Entities/Business User/BusinessUser.cs
@@ -8,10 +8,12 @@ public class BusinessUser : BaseEntity
     public string DisplayName { get; set; }
 
     public string Email { get; set; }
-
+    
     public string NormalizedEmail { get; set; }
 
     public string PhoneNumber { get; set; }
+
+    public string Role { get; set; }
 
     public ICollection<TripRequest?> ManagerTripRequest { get; set; } = new HashSet<TripRequest?>();
 
