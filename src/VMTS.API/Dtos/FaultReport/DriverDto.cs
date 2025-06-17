d65cc9149c51ac1f6add515de651c9f2e6056fd2
Omar Mohamed <omarmohamedx4e@gmail.com>
2025-06-18 00:11:03 +0300

    required dto

---
+++ src/VMTS.API/Dtos/FaultReport/DriverDto.cs
@@ -0,0 +1,7 @@
+namespace VMTS.API.Dtos;
+
+public class DriverDto
+{
+    public string Id { get; set; }
+    public string DisplayName { get; set; }
+}
---
+++ src/VMTS.API/Dtos/FaultReport/VehicleDto.cs
@@ -0,0 +1,7 @@
+namespace VMTS.API.Dtos;
+
+public class VehicleDto
+{
+    public string Id { get; set; }
+    public string PalletNumber { get; set; }
+}
