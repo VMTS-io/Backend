099dfe895cf7426eb39ef3adbab46790cb60afd4
Omar Mohamed <omarmohamedx4e@gmail.com>
2025-06-21 03:12:38 +0300

    add VehicleModelDto to VehicleDto and refactor pagination logic in VehicleSpecParams

--- src/VMTS.API/Dtos/FaultReport/VehicleDto.cs
+++ src/VMTS.API/Dtos/FaultReport/VehicleDto.cs
@@ -1,7 +1,11 @@
+using VMTS.API.Dtos.Vehicles.Category;
+using VMTS.API.Dtos.Vehicles.Model;
+
 namespace VMTS.API.Dtos;
 
 public class VehicleDto
 {
     public string Id { get; set; }
     public string PalletNumber { get; set; }
+    public VehicleModelDto VehicleModelDto { get; set; }
 }
