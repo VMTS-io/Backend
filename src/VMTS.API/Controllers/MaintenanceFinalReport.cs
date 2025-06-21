f6c0b05eb530ff530c6f7124397a56ba670a3c83
Omar Mohamed <omarmohamedx4e@gmail.com>
2025-06-21 03:10:44 +0300

    add role-based authorization to MaintenanceFinalReport and MaintenanceInitialReport controllers

--- src/VMTS.API/Controllers/MaintenanceFinalReport.cs
+++ src/VMTS.API/Controllers/MaintenanceFinalReport.cs
@@ -1,8 +1,10 @@
 using System.Security.Claims;
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using VMTS.API.Dtos.Maintenance.Report.Final;
 using VMTS.Core.Entities.Maintenace;
+using VMTS.Core.Helpers;
 using VMTS.Core.Interfaces.Services;
 using VMTS.Core.Specifications.Maintenance.Report.Final;
 
@@ -22,6 +24,7 @@ public class MaintenanceFinalReportController : BaseApiController
     }
 
     #region Create
+    [Authorize(Roles = Roles.Mechanic)]
     [HttpPost]
     public async Task<IActionResult> Create([FromBody] MaintenanceFinalReportRequestDto dto)
     {
@@ -34,6 +37,7 @@ public class MaintenanceFinalReportController : BaseApiController
     #endregion
 
     #region Update
+    [Authorize(Roles = $"{Roles.Mechanic},{Roles.Manager}")]
     [HttpPut("{id}")]
     public async Task<IActionResult> Update(
         [FromRoute] string id,
@@ -48,6 +52,7 @@ public class MaintenanceFinalReportController : BaseApiController
     #endregion
 
     #region Delete
+    [Authorize(Roles = $"{Roles.Mechanic},{Roles.Manager}")]
     [HttpDelete("{id}")]
     public async Task<IActionResult> Delete([FromRoute] string id)
     {
@@ -57,6 +62,7 @@ public class MaintenanceFinalReportController : BaseApiController
     #endregion
 
     #region Get By Id
+    [Authorize(Roles = $"{Roles.Mechanic},{Roles.Manager}")]
     [HttpGet("{id}")]
     public async Task<ActionResult<MaintenanceFinalReportResponseDto>> GetById(
         [FromRoute] string id
@@ -69,6 +75,7 @@ public class MaintenanceFinalReportController : BaseApiController
     #endregion
 
     #region GetAll
+    [Authorize(Roles = $"{Roles.Mechanic},{Roles.Manager}")]
     [HttpGet]
     public async Task<ActionResult<IReadOnlyList<MaintenanceFinalReportResponseDto>>> GetAll(
         [FromQuery] MaintenanceFinalReportSpecParams specParams
