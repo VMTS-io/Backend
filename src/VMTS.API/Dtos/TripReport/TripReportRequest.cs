543e4665b5ce6b2d5e723864206ebd09b5ee4ebd
Omar Mohamed <omarmohamedx4e@gmail.com>
2025-06-22 23:08:28 +0300

    add TripReport feature: implement TripReport service, DTOs, and controller with CRUD operations and user-specific handling

---
+++ src/VMTS.API/Dtos/TripReport/TripReportRequest.cs
@@ -0,0 +1,8 @@
+namespace VMTS.API.Dtos.TripReport;
+
+public class TripReportRequest
+{
+    public string Details { get; set; }
+    public int FuelRefile { get; set; }
+    public decimal Cost { get; set; }
+}
---
+++ src/VMTS.API/Dtos/TripReport/TripReportResponse.cs
@@ -0,0 +1,20 @@
+namespace VMTS.API.Dtos.TripReport;
+
+public class TripReportResponse
+{
+    public string Id { get; set; }
+
+    public DriverDto Driver { get; set; }
+
+    public VehicleDto vehicle { get; set; }
+
+    public string Details { get; set; }
+
+    public int FuelRefile { get; set; }
+
+    public decimal FuelCost { get; set; }
+
+    public TripDto Trip { get; set; }
+
+    public DateTime ReportedAt { get; set; }
+}
