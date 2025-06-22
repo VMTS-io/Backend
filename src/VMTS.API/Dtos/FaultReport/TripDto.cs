99145bfcdf26de9d05d2fa4cdad26aa4558534b5
Omar Mohamed <omarmohamedx4e@gmail.com>
2025-06-22 23:08:44 +0300

    add Type property to TripDto to include trip classification

--- src/VMTS.API/Dtos/FaultReport/TripDto.cs
+++ src/VMTS.API/Dtos/FaultReport/TripDto.cs
@@ -1,7 +1,10 @@
+using VMTS.Core.Entities.Trip;
+
 namespace VMTS.API.Dtos;
 
 public class TripDto
 {
     public string Id { get; set; }
     public string Destination { get; set; }
+    public TripType Type { get; set; }
 }
