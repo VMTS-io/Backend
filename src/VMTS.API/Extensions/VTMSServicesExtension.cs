233e99f1ae14ab6910473a3e423fb0e55b787e4f
Omar Mohamed <omarmohamedx4e@gmail.com>
2025-06-25 20:53:11 +0300

    register Identity-related services and entities in VTMSServicesExtension

--- src/VMTS.API/Extensions/VTMSServicesExtension.cs
+++ src/VMTS.API/Extensions/VTMSServicesExtension.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.Json.Serialization;
 using FluentValidation;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using VMTS.API.ActionFilters;
@@ -7,6 +8,7 @@ using VMTS.API.Errors;
 using VMTS.API.GlobalExceptionHnadler;
 using VMTS.API.Helpers;
 using VMTS.API.Middlewares;
+using VMTS.Core.Entities.Identity;
 using VMTS.Core.Interfaces.Repositories;
 using VMTS.Core.Interfaces.Services;
 using VMTS.Core.Interfaces.UnitOfWork;
