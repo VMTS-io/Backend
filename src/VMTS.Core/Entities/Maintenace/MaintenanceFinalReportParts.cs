30bc231de89f6577cae166cea91ae81cdc8911de
Omar Mohamed <omarmohamedx4e@gmail.com>
2025-06-27 00:34:36 +0300

    add new properties to MaintenanceFinalReportParts and update relationships for Vehicle and FinalReport

--- src/VMTS.Core/Entities/Maintenace/MaintenanceFinalReportParts.cs
+++ src/VMTS.Core/Entities/Maintenace/MaintenanceFinalReportParts.cs
@@ -1,12 +1,20 @@
 using VMTS.Core.Entities.Parts;
+using VMTS.Core.Entities.Vehicle_Aggregate;
 
 namespace VMTS.Core.Entities.Maintenace;
 
 public class MaintenanceFinalReportParts : BaseEntity
 {
-    public string MaintnenanceFinalReportId { get; set; } = default!;
-    public MaintenanceFinalReport FinalReport { get; set; } = default!;
-    public string PartId { get; set; } = default!;
-    public Part Part { get; set; } = default!;
+    public DateTime ChangedAt { get; set; }
+
+    public int KMAtChange { get; set; }
     public int Quantity { get; set; } = default!;
+
+    public string? VehicleId { get; set; }
+    public string MaintnenanceFinalReportId { get; set; } = default!;
+    public string PartId { get; set; } = default!;
+
+    public Vehicle Vehicle { get; set; }
+    public Part Part { get; set; } = default!;
+    public MaintenanceFinalReport FinalReport { get; set; } = default!;
 }
