889b409fd4eb4db1ab36cca0e01b4a1e2a7a9e7a
Omar Mohamed <omarmohamedx4e@gmail.com>
2025-07-01 04:15:16 +0300

    Remove unused properties from `VehicleTrackingDto`

--- src/VMTS.API/Dtos/MaintenanceTrackingForGetVehicleInDue/VehicleTrackingDto.cs
+++ src/VMTS.API/Dtos/MaintenanceTrackingForGetVehicleInDue/VehicleTrackingDto.cs
@@ -10,13 +10,6 @@ public class VehicleTrackingDto
 
     public string Status { get; set; }
 
-    public DateTime? LastChangedDate { get; set; }
-    public DateTime? NextChangeDate { get; set; } = DateTime.Now;
-
-    public int LastReplacedAtKm { get; set; }
-    public int NextChangeKm { get; set; }
-    public int CurrentKm { get; set; }
-
     public string ModelName { get; set; }
     public string BrandName { get; set; }
     public string CategoryName { get; set; }
