d8afcbcd54d3ddd308da57a8ad93538ca6583e2f
Omar Mohamed <omarmohamedx4e@gmail.com>
2025-07-01 04:02:09 +0300

    Add `VehicleWithDuePartsDto` for encapsulating vehicle details with due parts

---
+++ src/VMTS.API/Dtos/MaintenanceTrackingForGetVehicleInDue/VehicleWithDuePartsDto.cs
@@ -0,0 +1,10 @@
+using VMTS.Core.Entities.Parts;
+
+namespace VMTS.API.Dtos.MaintenanceTrackingForGetVehicleInDue;
+
+public class VehicleWithDuePartsDto
+{
+    public string VehicleId { get; set; }
+    public string PlateNumber { get; set; }
+    public List<DuePart> DueParts { get; set; }
+}
