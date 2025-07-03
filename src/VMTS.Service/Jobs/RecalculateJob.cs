6358fe38f48f350d8ecbfcc19044b3af768722b3
Omar Mohamed <omarmohamedx4e@gmail.com>
2025-07-03 20:40:51 +0300

    Add `RecalculateJob` with `RunRecalculateAll` method to trigger maintenance data recalculation

---
+++ src/VMTS.Service/Jobs/RecalculateJob.cs
@@ -0,0 +1,19 @@
+using VMTS.API.Dtos.MaintenanceTrackingForGetVehicleInDue;
+using VMTS.Core.Interfaces.Services;
+
+namespace VMTS.Repository.Data.Jobs;
+
+public class RecalculateJob
+{
+    private readonly IMaintenanceTrackingService _maintenanceService;
+
+    public RecalculateJob(IMaintenanceTrackingService maintenanceService)
+    {
+        _maintenanceService = maintenanceService;
+    }
+
+    public async Task RunRecalculateAll()
+    {
+        await _maintenanceService.RecalculateAllAsync();
+    }
+}
