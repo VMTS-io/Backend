2a99f14b47d06b144d05392880ed980689319011
Bassel Raafat <basselr11@gmail.com>
2025-07-04 21:27:12 +0300

    HangFire

--- src/VMTS.API/Helpers/HangfireDashboardAuthFilter.cs
+++ src/VMTS.API/Helpers/HangfireDashboardAuthFilter.cs
@@ -4,12 +4,12 @@ namespace VMTS.API.Middlewares;
 
 public class HangfireDashboardAuthFilter : IDashboardAuthorizationFilter
 {
-    public bool Authorize(DashboardContext context)
-    {
-        var httpContext = context.GetHttpContext();
+    public bool Authorize(DashboardContext context) => true;
+   // {
+       // var httpContext = context.GetHttpContext();
 
         // Example: Only allow logged-in users with Admin role
-        return httpContext.User.Identity?.IsAuthenticated == true
-            && httpContext.User.IsInRole("Admin");
-    }
+      //  return httpContext.User.Identity?.IsAuthenticated == true
+     //       && httpContext.User.IsInRole("Admin");
+    //}
 }
