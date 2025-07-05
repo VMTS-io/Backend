0cc5a95e05a3c32f7f0e21b78d6527f7a88e4f43
Bassel Raafat <basselr11@gmail.com>
2025-07-05 05:10:31 +0300

    Add vlidators for update dto in final and initial reports

--- src/VMTS.API/Validators/MaintenanceInitialReportRequestDtoValidator.cs
+++ src/VMTS.API/Validators/MaintenanceInitialReportRequestDtoValidator.cs
@@ -1,34 +1,32 @@
 using FluentValidation;
 using VMTS.API.Dtos.Maintenance.Report.Initial;
 
-namespace VMTS.API.Validators
+namespace VMTS.API.Validators;
+
+public class MaintenanceInitialReportRequestDtoValidator
+    : AbstractValidator<MaintenanceInitialReportRequestDto>
 {
-    public class MaintenanceInitialReportRequestDtoValidator
-        : AbstractValidator<MaintenanceInitialReportRequestDto>
+    public MaintenanceInitialReportRequestDtoValidator()
     {
-        public MaintenanceInitialReportRequestDtoValidator()
-        {
-            RuleFor(x => x.Notes)
-                // .NotEmpty()
-                // .WithMessage("Notes are required.")
-                .MaximumLength(1000)
-                .WithMessage("Notes must not exceed 1000 characters.");
+        RuleFor(x => x.Notes)
+            // .NotEmpty()
+            // .WithMessage("Notes are required.")
+            .MaximumLength(1000)
+            .WithMessage("Notes must not exceed 1000 characters.");
 
-            RuleFor(x => x.ExpectedFinishDate)
-                .GreaterThanOrEqualTo(DateOnly.FromDateTime(DateTime.UtcNow.Date))
-                .WithMessage("Expected finish date cannot be in the past.");
+        RuleFor(x => x.ExpectedFinishDate)
+            .GreaterThanOrEqualTo(DateOnly.FromDateTime(DateTime.UtcNow.Date))
+            .WithMessage("Expected finish date cannot be in the past.");
 
-            RuleFor(x => x.MaintenanceRequestId)
-                .NotEmpty()
-                .WithMessage("MaintenanceRequestId is required.");
+        RuleFor(x => x.MaintenanceRequestId)
+            .NotEmpty()
+            .WithMessage("MaintenanceRequestId is required.");
 
-            RuleFor(x => x.ExpectedChangedParts)
-                .NotNull()
-                .WithMessage("ExpectedChangedParts list is required.");
+        RuleFor(x => x.ExpectedChangedParts)
+            .NotNull()
+            .WithMessage("ExpectedChangedParts list is required.");
 
-            RuleForEach(x => x.ExpectedChangedParts)
-                .SetValidator(new MaintenanceReportPartDtoValidator());
-        }
+        RuleForEach(x => x.ExpectedChangedParts)
+            .SetValidator(new MaintenanceReportPartDtoValidator());
     }
 }
-
